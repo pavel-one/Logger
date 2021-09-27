434620267e0e7ad47c0257bd171ed5b81ac4d6fc
Pavel Zarubin <pavel@orendat.ru>
2021-09-27 17:14:44 +0300

    Laravel Echo Server

--- node/js/app.js
+++ node/js/app.js
@@ -1,3 +1,16 @@
 console.log('INJECTED')
 
 
+import Echo from "laravel-echo";
+
+window.io = require('socket.io-client');
+
+window.Echo = new Echo({
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':6001'
+});
+
+window.Echo.channel('notification.logger')
+    .listen('logger.add', (e) => {
+        console.log(e);
+    });
