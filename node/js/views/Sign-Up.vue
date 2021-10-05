40d678cc00599adcd6d58648dc0f7106adfae75b
Pavel Zarubin <pavel@orendat.ru>
2021-10-05 23:25:46 +0300

    Шаблон регистрации

--- node/js/views/Sign-Up.vue
+++ node/js/views/Sign-Up.vue
@@ -1,37 +1,30 @@
-<!-- 
-	This is the sign up page, it uses the dashboard layout in: 
-	"./layouts/Default.vue" .
- -->
-
 <template>
 	<div>
 
-		<!-- Sign Up Image And Headings -->
-		<div class="sign-up-header" style="background-image: url('images/bg-signup.jpg')">
+		<div class="sign-up-header" >
 			<div class="content">
-				<h1 class="mb-5">Sign Up</h1>
-				<p class="text-lg">Use these awesome forms to login or create new account in your project for free.</p>
+				<h1 class="mb-5">Регистрация</h1>
+				<p class="text-lg">
+                    Для возможности использовать наш сервис - вам необходимо зарегистрироваться, не переживайте, это бесплатно и
+                    <br> <b>всегда будет так</b>
+                </p>
 			</div>
 		</div>
-		<!-- / Sign Up Image And Headings -->
-		
+
 		<!-- Sign Up Form -->
 		<a-card :bordered="false" class="card-signup header-solid h-full" :bodyStyle="{paddingTop: 0}">
 			<template #title>
-				<h5 class="font-semibold text-center">Register With</h5>
+				<h5 class="font-semibold text-center">Регистрация</h5>
 			</template>
 			<div class="sign-up-gateways">
     			<a-button>
-					<img src="images/logos/logos-facebook.svg" alt="">
+                    <a-icon type="github" />
 				</a-button>
     			<a-button>
-					<img src="images/logos/logo-apple.svg" alt="">
-				</a-button>
-    			<a-button>
-					<img src="images/logos/Google__G__Logo.svg.png" alt="">
+                    <a-icon type="google" />
 				</a-button>
 			</div>
-			<p class="text-center my-25 font-semibold text-muted">Or</p>
+			<p class="text-center my-25 font-semibold text-muted">или</p>
 			<a-form
 				id="components-form-demo-normal-login"
 				:form="form"
@@ -79,16 +72,16 @@
 						},
 						]"
 					>
-						I agree the <a href="#" class="font-bold text-dark">Terms and Conditions</a>
+						Я согласен на все согласия
 					</a-checkbox>
 				</a-form-item>
 				<a-form-item>
 					<a-button type="primary" block html-type="submit" class="login-form-button">
-						SIGN UP
+						Регистрация
 					</a-button>
 				</a-form-item>
 			</a-form>
-			<p class="font-semibold text-muted text-center">Already have an account? <router-link to="/sign-in" class="font-bold text-dark">Sign In</router-link></p>
+			<p class="font-semibold text-muted text-center">Уже есть аккаунт? <router-link to="/sign-in" class="font-bold text-dark">Войти</router-link></p>
 		</a-card>
 		<!-- / Sign Up Form -->
 
--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('log')->group(function () {
-
-});
-
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register'])->name('register');
 });
